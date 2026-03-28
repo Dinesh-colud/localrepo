f5b6cfe77d88594af2cd23dc5ced95d7d776e069
Dinesh Rajbhar <dineshrajbhar9595066@gmail.com>
2026-03-28 18:03:07 +0530

    java Changed2

--- DineshRepo/index.java
+++ DineshRepo/index.java
@@ -17,35 +17,35 @@ public class index {
     public static void main(String[] args) {
         System.out.println("Hello Dinesh");
 
-        Scanner sc = new Scanner(System.in);
-        int choice;
-        
-        do{
-            System.out.println("1. Add");
-            System.out.println("2. Sub");
-            System.out.println("3. mul");
-            System.out.println("4. divide");
-            System.out.println("5. Exit");
-            System.out.println("Enter your choice : ");
-            choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println("Enter First Number: ");
-                    int a = sc.nextInt();
-                    System.out.println("Enter Second Number: ");
-                    int b = sc.nextInt();
-                    System.out.println("Sum: "+sum(a,b));
-                    break;
-                case 2:   
-                    System.out.println("Enter First Number: ");
-                    int c = sc.nextInt();
-                    System.out.println("Enter Second Number: ");
-                    int d = sc.nextInt(); 
+        try (Scanner sc = new Scanner(System.in)) {
+            int choice;
             
-                default:
-                    break;
-            }
-        }while(choice != 5);
-
+            do{
+                System.out.println("1. Add");
+                System.out.println("2. Sub");
+                System.out.println("3. mul");
+                System.out.println("4. divide");
+                System.out.println("5. Exit");
+                System.out.println("Enter your choice : ");
+                choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter First Number: ");
+                        int a = sc.nextInt();
+                        System.out.println("Enter Second Number: ");
+                        int b = sc.nextInt();
+                        System.out.println("Sum: "+sum(a,b));
+                        break;
+                    case 2:   
+                        System.out.println("Enter First Number: ");
+                        int c = sc.nextInt();
+                        System.out.println("Enter Second Number: ");
+                        int d = sc.nextInt(); 
+                
+                    default:
+                        break;
+                }
+            }while(choice != 5);
+        }
     }
 }
